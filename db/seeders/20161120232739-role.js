81013a289242788f39488bb290ef40ff25800ded
Joshua Azemoh <joshazemoh@gmail.com>
2016-12-04 20:21:19 +0100

    data(seed): create db seed data

--- db/seeders/20161120232739-role.js
+++ db/seeders/20161120232739-role.js
@@ -2,10 +2,12 @@ module.exports = {
   up(queryInterface) {
     return queryInterface.bulkInsert('Roles', [
       {
+        id: 1,
         title: 'admin',
         createdAt: new Date(),
         updatedAt: new Date()
       }, {
+        id: 2,
         title: 'regular',
         createdAt: new Date(),
         updatedAt: new Date()
