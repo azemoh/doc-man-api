b33a8dd573339fe284b887e13dee887b08e4603a
Joshua Azemoh <joshazemoh@gmail.com>
2017-02-02 20:29:56 +0100

    feature(cors): enable cross origin requests

--- config/app.js
+++ config/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const parser = require('body-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 app.use(logger('dev'));
